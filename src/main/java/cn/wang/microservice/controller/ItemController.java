879d080367d778cc6a4fee31d36ff3440377b015
WangChao211 <565276012@qq.com>
2017-12-14 14:03:14 +0800

    commit

--- src/main/java/cn/wang/microservice/controller/ItemController.java
+++ src/main/java/cn/wang/microservice/controller/ItemController.java
@@ -3,14 +3,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 import cn.wang.microservice.pojo.Item;
+import cn.wang.microservice.pojo.JdbcConfigBean;
 import cn.wang.microservice.service.ItemService;
 @RestController
 public class ItemController {
 	@Autowired
 	ItemService service;
+	
+	@Autowired
+	JdbcConfigBean jdbcConfigBean;
+	
 	@GetMapping(value="/item/{id}")
 	public Item queryById(@PathVariable("id") Long id){
 		return service.queryItemById(id);
+	}
+	
+	@GetMapping(value="test")
+	public String test(){
+		return jdbcConfigBean.toString();
 	}
 }
